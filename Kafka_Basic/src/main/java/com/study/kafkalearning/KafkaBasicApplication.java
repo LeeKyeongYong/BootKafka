82cad7754d0f1415ca21864217a4c1d16ad372de
이경용 <sleekydz86@icloud.com>
2023-12-23 01:21:30 +0900

    카프카 베이직 셋팅
    카프카 베이직 셋팅

---
+++ Kafka_Basic/src/main/java/com/study/kafkalearning/KafkaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.study.kafkalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBasicApplication.class, args);
+    }
+
+}
---
+++ Kafka_Basic/src/test/java/com/study/kafkalearning/KafkaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kafkalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
