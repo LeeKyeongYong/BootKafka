f4a5e1e759b3eebb4e008d70fa00ce11efbc6e7c
이경용 <sleekydz86@icloud.com>
2023-12-24 00:56:01 +0900

    import제거
    import제거 kafak베이직 정리완료

--- Kafka_Basic/src/main/java/com/study/kafkalearning/controller/KafkaProducerController.java
+++ Kafka_Basic/src/main/java/com/study/kafkalearning/controller/KafkaProducerController.java
@@ -5,7 +5,6 @@ import com.study.kafkalearning.service.KafakaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
