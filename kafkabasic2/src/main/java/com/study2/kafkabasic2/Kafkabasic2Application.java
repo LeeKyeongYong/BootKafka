0534b628ab2e57106cdbde4383dd4a9f6ea17129
이경용 <sleekydz86@icloud.com>
2024-02-27 12:31:14 +0900

    카프카 두번째 셋팅

---
+++ kafkabasic2/src/main/java/com/study2/kafkabasic2/Kafkabasic2Application.java
@@ -0,0 +1,13 @@
+package com.study2.kafkabasic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafkabasic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafkabasic2Application.class, args);
+    }
+
+}
---
+++ kafkabasic2/src/test/java/com/study2/kafkabasic2/Kafkabasic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study2.kafkabasic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafkabasic2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
